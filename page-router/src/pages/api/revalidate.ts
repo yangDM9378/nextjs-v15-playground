6e7a8173560c255e7518401a2f2440b4dd6d08a3
yangDM9378 <coco1622555@gmail.com>
2026-02-22 14:13:05 +0900

    feat: on-dement ISR

---
+++ page-router/src/pages/api/revalidate.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await res.revalidate("/")
+    return res.json({revalidate: true});
+  } catch (err) {
+    res.status(500).send('Revalidateion Faled')
+  }
+}
--- page-router/src/pages/index.tsx
+++ page-router/src/pages/index.tsx
@@ -14,7 +14,6 @@ export const getStaticProps = async () => {
       allBooks,
       recoBooks
     },
-    revalidate: 3,
   }
 };
 
