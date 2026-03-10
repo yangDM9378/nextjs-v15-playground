eabb9b80d171de699c5644c9e82dfa5d44c04496
yangDM9378 <coco1622555@gmail.com>
2026-03-10 20:37:59 +0900

    feat: React Server Component & Navigation

--- app-router/src/app/(with-sarchbar)/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default async function Page({searchParams,}:{saarchParams: Promise<{q:string}>}) {
-  
-  const {q} = await searchParams
-  return (
-    <div>Search 페이지 : {q}</div>
-  )
-}
---
+++ app-router/src/components/server-component.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ServerComponent() {
+  return (
+    <div>server-component</div>
+  )
+}
