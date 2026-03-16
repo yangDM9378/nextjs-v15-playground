6ed3e7763129cdd8949d58f42139ba05312f99cd
yangDM9378 <coco1622555@gmail.com>
2026-03-16 21:29:39 +0900

    feat: data cache & Request Memoization

--- app-router/src/app/(with-searchbar)/page.tsx
+++ app-router/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,46 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
-export default function Home() {
+async function Allbooks () {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {cache:"no-store"})
+  if (!response.ok) {
+    return <div>오류가 발생했습니다....</div>
+  }
+  const allBooks : BookData[] = await response.json();
+
+  return <div>
+    {allBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+
+async function RecoBooks () {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, {next: {revalidate:10}})
+  if (!response.ok) {
+    return <div>오류가 발생했습니다....</div>
+  }
+  const recoBooks : BookData[] = await response.json();
+
+  return <div>
+    {recoBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks/>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <Allbooks/>
       </section>
     </div>
   );
--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -1,6 +1,22 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response  = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`)
+  if (!response.ok) {
+    return <footer>제작 @dmyang</footer>
+  }
+
+  const books : BookData[] = await response.json()
+  const bookCount = books.length;
+
+  return <footer>
+    <div>제작 @dmyang</div>
+    <div>{bookCount}개의 도서가 있습니다.</div>
+  </footer>
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +31,7 @@ export default function RootLayout({
             <Link href={"/"}>BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @dongmin</footer>
+          <Footer/>
         </div>
       </body>
     </html>
