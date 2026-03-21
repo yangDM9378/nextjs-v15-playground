47cd2def1a22022398d824d7e84bcb2f37fd4ad2
yangDM9378 <coco1622555@gmail.com>
2026-03-21 17:45:34 +0900

    feat: error handling

--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -4,17 +4,17 @@ import style from "./layout.module.css";
 import { BookData } from "@/types";
 
 async function Footer() {
-  const response  = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {cache: "force-cache"})
-  if (!response.ok) {
-    return <footer>제작 @dmyang</footer>
-  }
+  // const response  = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {cache: "force-cache"})
+  // if (!response.ok) {
+  //   return <footer>제작 @dmyang</footer>
+  // }
 
-  const books : BookData[] = await response.json()
-  const bookCount = books.length;
+  // const books : BookData[] = await response.json()
+  // const bookCount = books.length;
 
   return <footer>
     <div>제작 @dmyang</div>
-    <div>{bookCount}개의 도서가 있습니다.</div>
+    {/* <div>{bookCount}개의 도서가 있습니다.</div> */}
   </footer>
 }
 
